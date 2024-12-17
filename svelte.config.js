fcdfba6942e927459fd675367572c6591f7c922c
ThommyWestlund <thommy.westlund@b3.se>
2024-12-17 22:13:52 +0100

    updated project to use azure adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
+//import adapter from "@sveltejs/adapter-auto";
+//import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +13,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: azure(),
   },
 };
 
