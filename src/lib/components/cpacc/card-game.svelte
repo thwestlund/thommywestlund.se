b020daa55ee690d77a814f6a0a229f287e865e81
ThommyWestlund <thommy.westlund@b3.se>
2025-01-09 19:48:29 +0100

    Added functionality to filter on WCAG questions only

--- src/lib/components/cpacc/card-game.svelte
+++ src/lib/components/cpacc/card-game.svelte
@@ -23,11 +23,27 @@
   }
 
   function randomizeQuestion(questions: IQuestion[]) {
+    if (
+      document.getElementById("wcag-cb") &&
+      (document.getElementById("wcag-cb") as HTMLInputElement).checked
+    ) {
+      questions = questions.filter((q) => q.category === "WCAG");
+    }
     return questions[Math.floor(Math.random() * questions.length)];
   }
 </script>
 
 <div class="flex flex-1 flex-col gap-8">
+  <div class="flex flex-row items-center gap-2">
+    <input
+      type="checkbox"
+      id="wcag-cb"
+      onclick={() => (randQuestion = randomizeQuestion(questions))}
+      class="h-4 w-4"
+    />
+    <label for="wcag-cb">Show WCAG questions only</label>
+  </div>
+
   <Card {front} question={randQuestion} />
 
   <div class="flex flex-row gap-4">
