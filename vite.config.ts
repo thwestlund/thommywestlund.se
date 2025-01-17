eff264d8404a9ab4f72734557e63f4dbb47862b0
ThommyWestlund <thommy.westlund@b3.se>
2025-01-17 09:06:55 +0100

    Removed define from vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
-  define: {
-    "process.env.PUBLIC_LINKEDIN": JSON.stringify(
-      "https://www.linkedin.com/in/thommy-westlund-02090650/",
-    ),
-  },
 });
